af62fded4b5d0755eacf4686e272709fc76d519c
Aabid Ali <mdaabidali28@gmail.com>
2025-05-18 16:32:40 +0530

    Best Time To BUy And Sell Stock II

---
+++ Top 150/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,19 @@
+
+
+class BestTimeToBuyAndSellStockII {
+    public int bestTimeToBuyAndSellStockII(int prices[]){
+        int maxProfit = 0;
+
+        for(int i=1; i<prices.length; i++){
+            if(prices[i] > prices[i-1]){
+                maxProfit = maxProfit + (prices[i] - prices[i-1]) ;
+            }
+        }
+        return  maxProfit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        BestTimeToBuyAndSellStockII obj = new BestTimeToBuyAndSellStockII();
+        System.out.println("Maximum Profit: " + obj.bestTimeToBuyAndSellStockII(prices));
+    }
+}
