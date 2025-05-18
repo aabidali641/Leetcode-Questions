607423c40149832161ce3dfbcbcc0c30983d7eaa
Aabid Ali <mdaabidali28@gmail.com>
2025-05-18 07:57:54 +0530

    Leetcode 169 . Majority Element

---
+++ Easy/MajorityElement.java
@@ -0,0 +1,25 @@
+class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+
+        MajorityElement obj = new MajorityElement();
+        int result = obj.majorityElement(nums);
+
+        System.out.println("Majority element is: " + result);
+    }
+}
