71e955fcd54ffcf85fd0f31c7e1d01fa899408d7
Aabid Ali <mdaabidali28@gmail.com>
2025-05-19 14:14:17 +0530

    Leetcode 45 . Jump Game

---
+++ Mediam/JumpGameII.java
@@ -0,0 +1,26 @@
+
+public class JumpGameII {
+
+    public static int jump(int[] nums) {
+        int jumps = 0;
+        int finalPosition = 0;
+        int idx = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            idx = Math.max(idx, i + nums[i]);
+
+            if (i == finalPosition) {
+                jumps++;
+                finalPosition = idx;
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+        int result = jump(nums);
+        System.out.println("Minimum jumps to reach end: " + result);
+    }
+}
